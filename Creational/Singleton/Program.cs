1bf5a4072aadd0cac85c2ce2706c1542d99e27fc
rachidamrani <rachid3alaoui@gmail.com>
2025-02-25 17:37:14 +0100

    removed unecessary code

--- Creational/Singleton/Program.cs
+++ Creational/Singleton/Program.cs
@@ -1,13 +1,5 @@
-﻿ParallelEnumerable.Range(0, 10000)
-.ForAll(_ =>
+﻿sealed class Singleton
 {
-    Singleton singleton = Singleton.Instance;
-});
-
-
-sealed class Singleton
-{
-    private static Singleton _instance = default!;
     private static readonly Lazy<Singleton> _lazySingleton = new Lazy<Singleton>(() => new Singleton());
 
     public static Singleton Instance => _lazySingleton.Value;
