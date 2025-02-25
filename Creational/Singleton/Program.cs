193cb6ab59a2377f95be916b4292c16cb3f0a76f
rachidamrani <rachid3alaoui@gmail.com>
2025-02-25 17:31:46 +0100

    naive singleton implementation with Thread Safety

---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,44 @@
+﻿// Many singletons would be created by each thread, which is also illegal!
+ParallelEnumerable.Range(0, 10000)
+.ForAll(_ =>
+{
+    Singleton singleton = Singleton.Instance;
+});
+
+
+// Console.WriteLine("Before accessing instance");
+
+// Singleton singleton1 = Singleton.Instance;
+
+// Console.WriteLine("After accessing instance");
+
+// Singleton singleton2 = Singleton.Instance; // Should be illegal
+
+sealed class Singleton
+{
+    private static Singleton _instance = default!;
+    private static object _lock = new();
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance is null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private Singleton()
+    {
+        Console.WriteLine("Instantiating Singleton ...");
+    }
+}
